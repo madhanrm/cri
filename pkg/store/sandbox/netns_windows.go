6a0394fd60859a94e6f88bf777fba7e85638a519
Justin Terry (VM) <juterry@microsoft.com>
2018-08-30 13:51:22 -0700

    Total hack remove NetNS on Windows
    Signed-off-by: Justin Terry (VM) <juterry@microsoft.com>

--- pkg/store/sandbox/netns_windows.go
+++ pkg/store/sandbox/netns_windows.go
@@ -1,4 +1,4 @@
-// +build !windows
+// +build windows
 
 /*
 Copyright 2017 The Kubernetes Authors.
@@ -19,14 +19,10 @@ limitations under the License.
 package sandbox
 
 import (
-	"os"
+	"math/rand"
 	"sync"
 
-	cnins "github.com/containernetworking/plugins/pkg/ns"
-	"github.com/docker/docker/pkg/symlink"
 	"github.com/pkg/errors"
-
-	osinterface "github.com/containerd/cri/pkg/os"
 )
 
 // ErrClosedNetNS is the error returned when network namespace is closed.
@@ -35,71 +31,36 @@ var ErrClosedNetNS = errors.New("network namespace is closed")
 // NetNS holds network namespace for sandbox
 type NetNS struct {
 	sync.Mutex
-	ns       cnins.NetNS
 	closed   bool
 	restored bool
+	path     string
 }
 
 // NewNetNS creates a network namespace for the sandbox
 func NewNetNS() (*NetNS, error) {
-	netns, err := cnins.NewNS()
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to setup network namespace")
-	}
-	n := new(NetNS)
-	n.ns = netns
-	return n, nil
+	// TODO: JTERRY75 - Plugin cni
+	b := make([]byte, 10)
+	rand.Read(b)
+	return &NetNS{path: string(b)}, nil
 }
 
 // LoadNetNS loads existing network namespace. It returns ErrClosedNetNS
 // if the network namespace has already been closed.
 func LoadNetNS(path string) (*NetNS, error) {
-	ns, err := cnins.GetNS(path)
-	if err != nil {
-		if _, ok := err.(cnins.NSPathNotExistErr); ok {
-			return nil, ErrClosedNetNS
-		}
-		if _, ok := err.(cnins.NSPathNotNSErr); ok {
-			// Do best effort cleanup.
-			os.RemoveAll(path) // nolint: errcheck
-			return nil, ErrClosedNetNS
-		}
-		return nil, errors.Wrap(err, "failed to load network namespace")
-	}
-	return &NetNS{ns: ns, restored: true}, nil
+	// TODO: JTERRY75 - Plugin cni
+	return &NetNS{restored: true, path: path}, nil
 }
 
 // Remove removes network namepace if it exists and not closed. Remove is idempotent,
 // meaning it might be invoked multiple times and provides consistent result.
 func (n *NetNS) Remove() error {
+	// TODO: JTERRY75 - Plugin cni
 	n.Lock()
 	defer n.Unlock()
 	if !n.closed {
-		err := n.ns.Close()
-		if err != nil {
-			return errors.Wrap(err, "failed to close network namespace")
-		}
 		n.closed = true
 	}
 	if n.restored {
-		path := n.ns.Path()
-		// Check netns existence.
-		if _, err := os.Stat(path); err != nil {
-			if os.IsNotExist(err) {
-				return nil
-			}
-			return errors.Wrap(err, "failed to stat netns")
-		}
-		path, err := symlink.FollowSymlinkInScope(path, "/")
-		if err != nil {
-			return errors.Wrap(err, "failed to follow symlink")
-		}
-		if err := osinterface.Unmount(path); err != nil && !os.IsNotExist(err) {
-			return errors.Wrap(err, "failed to umount netns")
-		}
-		if err := os.RemoveAll(path); err != nil {
-			return errors.Wrap(err, "failed to remove netns")
-		}
 		n.restored = false
 	}
 	return nil
@@ -116,12 +77,5 @@ func (n *NetNS) Closed() bool {
 func (n *NetNS) GetPath() string {
 	n.Lock()
 	defer n.Unlock()
-	return n.ns.Path()
-}
-
-// GetNs returns the network namespace handle
-func (n *NetNS) GetNs() cnins.NetNS {
-	n.Lock()
-	defer n.Unlock()
-	return n.ns
+	return n.path
 }
